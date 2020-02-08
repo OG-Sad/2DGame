a381a21d7cb1282821017c6d87232edd7aaa615c
Daw <daw2744@icloud.com>
2020-02-08 11:33:25 -0800

    Added Background

---
+++ 2DGame/Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+	public Transform Player;
+	public Transform BackgroundSpeed;
+    Material material;
+    Vector2 offset;
+    public float xVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector2(xVelocity, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		GameObject Speed = GameObject.Find("Player");
+		BackgroundSpeed.position = new Vector3(Speed.transform.position.x, 0f, 10f);
+        material.mainTextureOffset += offset * Time.deltaTime * .25f;
+    }
+}
