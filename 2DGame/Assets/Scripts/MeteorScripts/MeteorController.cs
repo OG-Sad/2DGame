904b1c7ebc9488f349ff4e333f49d793b3a71a1e
Unknown <nguyenduyjohn180@gmail.com>
2020-02-10 18:35:49 -0800

    warning and meteors system

---
+++ 2DGame/Assets/Scripts/MeteorScripts/MeteorController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MeteorController : MonoBehaviour
+{
+
+    float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //moves meteor to the left over time
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        //rotate meteor
+        //transform.Rotate(new Vector3(60, 0, -10) * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Planet")
+        {
+            //do nothing for now
+        }
+        else if (coll.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("PlanetSpawningTest");
+        }
+    }
+}
