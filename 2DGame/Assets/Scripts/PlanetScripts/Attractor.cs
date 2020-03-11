3b3ab2f9bb5249f2d58b7553066532fee999e3d6
Tobias <tobyma23@gmail.com>
2020-03-11 11:08:20 -0700

    fixed orbit ring and improved orbiting

--- 2DGame/Assets/Scripts/PlanetScripts/Attractor.cs
+++ 2DGame/Assets/Scripts/PlanetScripts/Attractor.cs
@@ -48,10 +48,7 @@ public class Attractor : MonoBehaviour
 
         if (PlanetRB.mass < .1)
         {
-            
-                orbittest = false;
-            
-
+            orbittest = false;
         }
 
         if (dist <= 5)
@@ -65,8 +62,6 @@ public class Attractor : MonoBehaviour
         lastVelocity = PlayerRB.velocity;
         lastPlayerPos = Player.transform.position;
         lastPlayPlanTan = PlayerPlanetTheta();
-
-        //Debug.Log(orbittest);
     }
 
     void Attract(GameObject objToAttract)
@@ -77,13 +72,8 @@ public class Attractor : MonoBehaviour
 
         // Added a repelling force if planet mass is 0
         float planetMass = !tapped ? PlanetRB.mass : zeroRepelForce; // <-- Repel force
-        //float planetMass = PlanetRB.mass;
         
 
-
-
-
-        
         // Could add future stuff when planet is at 0 mass here
         if (planetMass < 0) {
             //Debug.Log("Success!");
@@ -95,44 +85,22 @@ public class Attractor : MonoBehaviour
         Vector2 force = direction.normalized * forceMagnitude;
         orbitVelNum = Mathf.Sqrt(PlanetRB.mass/distance);
 
-
-        //checks to see if player is orbiting the first planet and turns off the gravity of the other planets
-        if (Mathf.Abs(PlayerRB.velocity.normalized.x + -direction.normalized.y) <= .5 && Mathf.Abs(PlayerRB.velocity.magnitude - orbitVelNum) <= .5 && firstPlanet==true)
-        {
-            Database.isOrbiting = true;            
-            Database.orbitPlanetPos = PlanetRB.position;
-            Attractor[] Planets = FindObjectsOfType<Attractor>();
-            foreach (Attractor Planet in Planets)
-            {
-                if (Planet != this)
-                {
-                    Planet.GetComponent<Attractor>().enabled = false;
-                }
-            }
-        }
-        else
-        {
-            Database.isOrbiting = false;
-        }
-
         //if the player is far enough away the orbit checking function gets turned off
         if (direction.magnitude > 5)
         {
             firstPlanet = false;
         }
-
-        //Debug.Log(Database.isOrbiting);
         PlayerRB.AddForce(force);
     }
 
     Vector2 Orbiting (Vector2 acc, float playTan, float dister) {
        
         Vector2 vel = PlayerRB.velocity;
-
+        
       
         float playPlanTan = PlayerPlanetTheta();
         float angleSum = Mathf.Abs(playTan - playPlanTan) / pi;
-
+        //Debug.Log(angleSum);
 
         Vector2 direction = PlanetRB.position - PlayerRB.position;
 
@@ -143,24 +111,35 @@ public class Attractor : MonoBehaviour
         // IMPORTANT: If we want to make the orbit eliptical, it'll be done here
         if((1.4 < angleSum && angleSum < 1.6) || (0.4 < angleSum && angleSum < 0.6)) {
 
-            Vector2 perpForce;
+            Vector2 perpForce= new Vector2(0,0);
 
             float forceX = force.x;
             float forceY = force.y;
 
-            if (playPlanTan - lastPlayPlanTan > 0) {
+
+            float tanDiff = playPlanTan - lastPlayPlanTan;
+
+            if (tanDiff > 0 && tanDiff < 1) {
+                
                 perpForce = new Vector2(forceY, -forceX);
                 
             }
-            else {
-                perpForce = new Vector2(-forceY, forceX);
+            else 
+            {
+                if (tanDiff < -1)
+                {
+                    perpForce = new Vector2(forceY, -forceX);
+                }
+                else
+                {
+                    perpForce = new Vector2(-forceY, forceX);
+                }
             }
 
             Vector2 orbitVel = perpForce.normalized * orbitVelNum;
-            Vector2 difference = vel + ((orbitVel - vel) / 10);
+            Vector2 difference = vel + ((orbitVel - vel) / 15);
 
-            //Mathf.Abs(orbitVel.magnitude-vel.magnitude)<=.5
-            if (orbitVel != vel) {
+            if (orbitVel != vel && Mathf.Abs(orbitVel.magnitude - vel.magnitude) <= 1) {
                 orbittest = true;
                 Player.GetComponent<Rigidbody2D>().velocity = difference;
             }
@@ -169,7 +148,7 @@ public class Attractor : MonoBehaviour
                 orbittest = false;
             }
         }
-        
+
         return PlayerRB.velocity;   
     }
 
@@ -181,8 +160,7 @@ public class Attractor : MonoBehaviour
         // Player Pos
         float m2X = Player.transform.position.x;
         float m2Y = Player.transform.position.y;
-
-      
+        
         return Mathf.Atan2(m2Y - m1Y, m2X - m1X);
     }
 
@@ -193,8 +171,6 @@ public class Attractor : MonoBehaviour
         float pVel = veloc.magnitude;
 
         float ringDist = (pMass / (pVel * pVel));
-
-        Debug.Log(pVel);
 
         OrbitRing.transform.localScale = new Vector3(ringDist, ringDist, 1);
     }
