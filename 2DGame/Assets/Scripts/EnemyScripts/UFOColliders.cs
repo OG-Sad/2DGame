a4e8896f2ecaf7a7f4461e609fbe1e415c74a337
Daw <daw2744@icloud.com>
2020-03-10 23:09:31 -0700

    same

--- 2DGame/Assets/Scripts/EnemyScripts/UFOColliders.cs
+++ 2DGame/Assets/Scripts/EnemyScripts/UFOColliders.cs
@@ -40,7 +40,12 @@ public class UFOColliders : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Player") && (!PowerUps.PlayerPoweredUp == true && PowerUps.ChoosePowerUp != 1))
+        if (other.gameObject.CompareTag("Player") && (!PowerUps.PlayerPoweredUp && PowerUps.ChoosePowerUp != 1))
+        {
+            Database.gameEnd = true;
+        }
+
+        if (!PowerUps.PlayerPoweredUp)
         {
             Database.gameEnd = true;
         }
