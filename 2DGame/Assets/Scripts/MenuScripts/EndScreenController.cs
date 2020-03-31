ea4db2e94c904f1cb3ffc6a404d464c2a36b3dbd
Unknown <nguyenduyjohn180@gmail.com>
2020-03-31 14:51:20 -0700

    online leaderboard system works

--- 2DGame/Assets/Scripts/MenuScripts/EndScreenController.cs
+++ 2DGame/Assets/Scripts/MenuScripts/EndScreenController.cs
@@ -10,6 +10,10 @@ public class EndScreenController : MonoBehaviour
     public GameObject endScreen;
     public GameObject distanceText;
     public GameObject highScoreText;
+    public GameObject mainMenuButton;
+    public GameObject playAgainButton;
+
+    List<int> top10Scores = new List<int>();
 
     // Start is called before the first frame update
     void Start()
@@ -27,10 +31,158 @@ public class EndScreenController : MonoBehaviour
 
             if (ScoreScript.scoreGlobal > PlayerPrefs.GetFloat("High Score"))
             {
+                //temporarilly set these buttons to false so player can't change scene while new high score is potentially uploading
+                mainMenuButton.SetActive(false);
+                playAgainButton.SetActive(false);
+
                 PlayerPrefs.SetFloat("High Score", ScoreScript.scoreGlobal);
+
+                //downloads the current high scores and sends the new one to the database if it is high enough
+                StartCoroutine("DownloadHighscoresFromDatabase");
             }
 
             highScoreText.GetComponent<Text>().text = "High Score: " + PlayerPrefs.GetFloat("High Score");
+        }
+    }
+
+    string GetUsername(int score) {
+        string username = "";
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (score > top10Scores[i])
+            {
+                print("score:" + score);
+                print("other:" + top10Scores[i]);
+                if (i == 0)
+                {
+                    username = "1";
+                    break;
+                }
+                else if (i == 1)
+                {
+                    username = "2";
+                    break;
+                }
+                else if (i == 2)
+                {
+                    username = "3";
+                    break;
+                }
+                else if (i == 3)
+                {
+                    username = "4";
+                    break;
+                }
+                else if (i == 4)
+                {
+                    username = "5";
+                    break;
+                }
+                else if (i == 5)
+                {
+                    username = "6";
+                    break;
+                }
+                else if (i == 6)
+                {
+                    username = "7";
+                    break;
+                }
+                else if (i == 7)
+                {
+                    username = "8";
+                    break;
+                }
+                else if (i == 8)
+                {
+                    username = "9";
+                    break;
+                }
+                else if (i == 9)
+                {
+                    username = "10";
+                    break;
+                }
+            }
+        }
+        return username;
+    }
+
+    //pushes back all of the high scores below the newest high score back one slot
+    void PushScoresBack(string username) {
+        int highScorePlace = int.Parse(username);
+
+        for (int i = highScorePlace + 1; i <= 10; i++) {
+            StartCoroutine(UploadNewHighscore(i.ToString(), top10Scores[i - 2]));
+        }
+    }
+
+    IEnumerator UploadNewHighscore(string username, int score)
+    {
+        const string privateCode = "g5IdeqN0PECOdCyRIVBqiAN8aJZjof5kmZyR_3i1Lrzg";
+        const string publicCode = "5e7d38eefe232612b8e47de9";
+        const string webURL = "http://dreamlo.com/lb/";
+
+        WWW www = new WWW(webURL + privateCode + "/add/" + WWW.EscapeURL(username) + "/" + score);
+        yield return www;
+
+        if (string.IsNullOrEmpty(www.error))
+            print("Upload Successful");
+        else
+        {
+            print("Error uploading: " + www.error);
+        }
+    }
+
+    //downloads the current high scores and sends the new one to the database if it is high enough
+    IEnumerator DownloadHighscoresFromDatabase()
+    { 
+        const string privateCode = "g5IdeqN0PECOdCyRIVBqiAN8aJZjof5kmZyR_3i1Lrzg";
+        const string publicCode = "5e7d38eefe232612b8e47de9";
+        const string webURL = "http://dreamlo.com/lb/";
+
+        WWW www = new WWW(webURL + publicCode + "/pipe/");
+        yield return www;
+
+        if (string.IsNullOrEmpty(www.error))
+        {
+            GetInfo(www.text);
+            //if the score is more than the 10th best score
+            if (ScoreScript.scoreGlobal > top10Scores[9])
+            {
+                string username = GetUsername((int)ScoreScript.scoreGlobal);
+                //the function is only necessary if the new high score is better than the 10th best
+                if (username != "10")
+                {
+                    PushScoresBack(username);
+                }
+                //uploads the new high score
+                StartCoroutine(UploadNewHighscore(username, (int)ScoreScript.scoreGlobal));
+
+                mainMenuButton.SetActive(true);
+                playAgainButton.SetActive(true);
+            }
+            else {
+                mainMenuButton.SetActive(true);
+                playAgainButton.SetActive(true);
+            }
+        }
+        else
+        {
+            print("Error Downloading: " + www.error);
+        }
+    }
+
+    void GetInfo(string textStream)
+    {
+        string[] entries = textStream.Split(new char[] { '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+        for (int i = 0; i < 10; i++)
+        {
+            string[] entryInfo = entries[i].Split(new char[] { '|' });
+            string username = entryInfo[0];
+            int score = int.Parse(entryInfo[1]);
+            top10Scores.Add(score);
         }
     }
 
--- 2DGame/Assets/Scripts/MenuScripts/LeaderboardController.cs
+++ 2DGame/Assets/Scripts/MenuScripts/LeaderboardController.cs
@@ -6,20 +6,74 @@ using UnityEngine.UI;
 public class LeaderboardController : MonoBehaviour
 {
 
-    public Text highScoreText;
+	const string privateCode = "g5IdeqN0PECOdCyRIVBqiAN8aJZjof5kmZyR_3i1Lrzg";
+	const string publicCode = "5e7d38eefe232612b8e47de9";
+	const string webURL = "http://dreamlo.com/lb/";
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        string highScore = PlayerPrefs.GetFloat("High Score").ToString();
+	public List<int> top10Scores = new List<int>();
 
-        highScoreText.text = "High Score: " + highScore;
-    }
+	public Text highScoreText;
 
-    //resets the PlayerPrefs high score
-    public void Reset() {
-        PlayerPrefs.DeleteAll();
+	GameObject highscoreSlot;
 
-        highScoreText.text = "High Score: 0";
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		highscoreSlot = GameObject.Find("Canvas/Panel/Top 10/");
+		highscoreSlot.SetActive(false);
+
+		string highScore = PlayerPrefs.GetFloat("High Score").ToString();
+
+		highScoreText.text = "High Score: " + highScore;
+
+		DownloadHighscores();
+	}
+
+	//resets the PlayerPrefs high score
+	public void Reset()
+	{
+		PlayerPrefs.DeleteAll();
+
+		highScoreText.text = "High Score: 0";
+	}
+
+	public void DownloadHighscores()
+	{
+		StartCoroutine("DownloadHighscoresFromDatabase");
+	}
+
+	IEnumerator DownloadHighscoresFromDatabase()
+	{
+		WWW www = new WWW(webURL + publicCode + "/pipe/");
+		yield return www;
+
+		if (string.IsNullOrEmpty(www.error))
+		{
+			FormatHighscores(www.text);
+
+			for (int i = 0; i < 10; i++)
+			{
+				GameObject highscoreSlot = GameObject.Find("Canvas/Panel/Top 10/" + (i + 1).ToString());
+				highscoreSlot.GetComponent<Text>().text = (i + 1).ToString() + ". " + top10Scores[i].ToString();
+			}
+
+			highscoreSlot.SetActive(true);
+		}
+		else
+		{
+			print("Error Downloading: " + www.error);
+		}
+	}
+
+	void FormatHighscores(string textStream)
+	{
+		string[] entries = textStream.Split(new char[] { '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+
+		for (int i = 0; i < 10; i++)
+		{
+			string[] entryInfo = entries[i].Split(new char[] { '|' });
+			int score = int.Parse(entryInfo[1]);
+			top10Scores.Add(score);
+		}
+	}
 }
